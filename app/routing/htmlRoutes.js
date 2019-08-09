46be8eb991a49723f86e032bae10d784566d1fbc
tejsubedi@gmail.com <tejsubedi@gmail.com>
2019-08-08 23:18:11 -0400

    app functionality added to htmlRoutes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
